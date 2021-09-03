3b374078259c72f056fa1f13ed767adaa9b0c816
hjpertuz <90066840+hjpertuz@users.noreply.github.com>
2021-09-03 16:41:02 -0500

    Add files via upload

---
+++ HolaMundo/src/MostrarPorConsola/Mostrar.java
@@ -0,0 +1,14 @@
+
+package MostrarPorConsola;
+
+
+public class Mostrar {
+
+   
+    public static void main(String[] args) {
+        
+        //System.out.println( "Hola mundo" );
+        Joptionpane.showMessageDialog(null,"hola mundo");
+    }
+    
+}
