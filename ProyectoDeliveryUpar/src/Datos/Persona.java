7681adff6a8e8b245e7ab333c04337c59fcf865f
hjpertuz <hjpertuz@unicesar.edu.co>
2021-10-30 18:50:49 -0500

    Proyecto

---
+++ ProyectoDeliveryUpar/src/Datos/Persona.java
@@ -0,0 +1,61 @@
+
+package Datos;
+
+public class Persona {
+    private String nombre;
+    private String direccion;
+    private String pais;
+    private String Ciudad;
+    private double telefono;
+    private String clave;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getCiudad() {
+        return Ciudad;
+    }
+
+    public void setCiudad(String Ciudad) {
+        this.Ciudad = Ciudad;
+    }
+
+    public double getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(double telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+    
+}
---
+++ ProyectoDeliveryUpar/src/Datos/Repartidor.java
@@ -0,0 +1,10 @@
+
+package Datos;
+
+
+public class Repartidor extends Persona{
+    
+    public void GestionPaquetesAsignados(){
+    
+    }
+}
